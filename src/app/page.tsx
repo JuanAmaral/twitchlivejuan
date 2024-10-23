56c9cd483cf95e3b0cc802a7bcd809f0ce7d8362
JuanAmaral <lgdfjktg@gmail.com>
2024-10-23 14:16:36 -0300

    update chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 export default function Home() {
   return (
     <div className="min-h-screen items-center justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <iframe
-        src="https://player.twitch.tv/?channel=juanamaral&parent=twitchlivejuan.vercel.app"
-        height="720"
-        width="100%"
-        allowFullScreen={true}
-      ></iframe>
+      <div className="flex gap-2 md:flex-row flex-col">
+        <iframe
+          src="https://player.twitch.tv/?channel=juanamaral&parent=twitchlivejuan.vercel.app"
+          height="720"
+          width="1280"
+          allowFullScreen={true}
+        ></iframe>
+        <iframe
+          src="https://www.twitch.tv/embed/juanamaral/chat?parent=twitchlivejuan.vercel.app"
+          height="500"
+          width="350"
+        ></iframe>
+      </div>
     </div>
   );
 }
